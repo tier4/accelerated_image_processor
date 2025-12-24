15f55bd685fbc9794c55edf35af46542f492b14a
Kotaro Uetake <60615504+ktro2828@users.noreply.github.com>
2025-12-24 12:32:35 +0900

    refactor: add common package (#16)

---
+++ src/accelerated_image_processor_common/include/accelerated_image_processor_common/datatype.hpp
@@ -0,0 +1,42 @@
+// Copyright 2025 TIER IV, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include <cstddef>
+#include <cstdint>
+#include <string>
+#include <vector>
+
+namespace accelerated_image_processor::common
+{
+/**
+ * @brief Enumeration of image formats.
+ */
+enum class ImageFormat : uint8_t { RGB, BGR };
+
+/**
+ * @brief Structure representing an image.
+ */
+struct Image
+{
+  std::string frame_id;       //!< Camera frame ID
+  uint64_t timestamp;         //!< Timestamp at the image is captured
+  uint32_t height;            //!< Image height, that is, number of rows
+  uint32_t width;             //!< Image width, that is, number of columns
+  uint32_t step;              //!< Full row length in bytes
+  ImageFormat format;         //!< Image format, either RGB or BGR
+  std::vector<uint8_t> data;  //!< Actual matrix data, size is (step * height)
+};
+}  // namespace accelerated_image_processor::common
---
+++ src/accelerated_image_processor_common/include/accelerated_image_processor_common/processor.hpp
@@ -0,0 +1,158 @@
+// Copyright 2025 TIER IV, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include "accelerated_image_processor_common/datatype.hpp"
+#include "accelerated_image_processor_common/parameter.hpp"
+
+#include <string>
+#include <type_traits>
+#include <utility>
+#include <variant>
+
+namespace accelerated_image_processor::common
+{
+/**
+ * @brief Base class for image processors.
+ */
+class BaseProcessor
+{
+private:
+  /**
+   * @brief A holder to wrap a invoking free function.
+   */
+  struct FreeFn
+  {
+    using invoker_t = void (*)(const Image &);
+    invoker_t invoker = nullptr;
+  };
+
+  /**
+   * @brief A holder to wrap a invoking member function.
+   */
+  struct MemberFn
+  {
+    using invoker_t = void (*)(void *, const Image &);
+    void * obj{};
+    invoker_t invoker = nullptr;
+  };
+
+  using FnStorage = std::variant<std::monostate, FreeFn, MemberFn>;
+
+  FnStorage storage_{std::monostate};  //!< Storage of the postprocess function.
+  ParameterMap parameters_;            //!< Parameters for the processor.
+
+  /**
+   * @brief Restore the member pointer from method_bits and invoke its method.
+   */
+  template <typename Obj, void (Obj::*Method)(const Image &)>
+  static void invoke_member(void * p, const Image & img)
+  {
+    auto * o = static_cast<Obj *>(p);
+    if (o) (o->*Method)(img);
+  }
+
+public:
+  /**
+   * @brief Constructor.
+   */
+  explicit BaseProcessor(ParameterMap parameters) : parameters_(std::move(parameters)) {}
+
+  virtual ~BaseProcessor() = default;
+
+  /**
+   * @brief Register a free function as the postprocess function.
+   * @param fn Free function for the postprocess function.
+   */
+  template <typename F, std::enable_if_t<std::is_convertible_v<F, FreeFn::invoker_t>, int> = 0>
+  void register_postprocess(F fn)
+  {
+    storage_ = FreeFn{static_cast<FreeFn::invoker_t>(fn)};
+  }
+
+  /**
+   * @brief Register a member function as the postprocess function.
+   */
+  template <typename Obj, void (Obj::*Method)(const Image &)>
+  void register_postprocess(Obj * obj)
+  {
+    storage_ = MemberFn{obj, &invoke_member<Obj, Method>};
+  }
+
+  /**
+   * @brief Process the input image.
+   * @param image The input image.
+   */
+  void process(const Image & image)
+  {
+    auto processed = this->process_impl(image);
+
+    std::visit(
+      [&processed](auto & f) {
+        using T = std::decay_t<decltype(f)>;
+        if constexpr (std::is_same_v<T, std::monostate>) {
+          // do nothing
+        } else if constexpr (std::is_same_v<T, FreeFn>) {
+          if (f.invoker) f.invoker(processed);
+        } else if constexpr (std::is_same_v<T, MemberFn>) {
+          if (f.invoker && f.obj) f.invoker(f.obj, processed);
+        }
+      },
+      storage_);
+  };
+
+  /**
+   * @brief Return the read/write reference to the parameters.
+   */
+  ParameterMap & parameters() { return parameters_; }
+
+  /**
+   * @brief Return the read-only reference to the parameters.
+   */
+  const ParameterMap & parameters() const { return parameters_; }
+
+  /**
+   * @brief Return the read/write reference to the parameter value.
+   * @tparam T The type of the parameter value.
+   * @param key The key of the parameter.
+   * @return The value of the parameter.
+   */
+  template <typename T>
+  T & parameter_value(const std::string & key)
+  {
+    return std::get<T>(parameters_.at(key));
+  }
+
+  /**
+   * @brief Return the read-only reference to the parameter value.
+   * @tparam T The type of the parameter value.
+   * @param key The key of the parameter.
+   * @return The value of the parameter.
+   */
+  template <typename T>
+  const T & parameter_value(const std::string & key) const
+  {
+    return std::get<T>(parameters_.at(key));
+  }
+
+protected:
+  /**
+   * @brief Process the input image.
+   * @param image The input image.
+   * @return The processed image.
+   */
+  virtual Image process_impl(const Image & image) = 0;
+};
+}  // namespace accelerated_image_processor::common
