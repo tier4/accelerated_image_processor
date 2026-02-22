df51508b269654117651dba6eb7297ff1ac13c5a
ktro2828 <kotaro.uetake@tier4.jp>
2026-02-22 13:30:11 +0900

    feat: add classmethod for image
    Signed-off-by: ktro2828 <kotaro.uetake@tier4.jp>

--- src/accelerated_image_processor_python/accelerated_image_processor/datatype/__init__.py
+++ src/accelerated_image_processor_python/accelerated_image_processor/datatype/__init__.py
@@ -1,6 +1,10 @@
 from __future__ import annotations
 
+from os import PathLike
+
 import accelerated_image_processor.accelerated_image_processor_python_datatype as datatype_cpp
+import cv2
+import numpy as np
 
 __all__ = [
     "ImageEncoding",
@@ -21,9 +25,59 @@ ImageFormat.__doc__ = """Enumeration of image compression formats."""
 DistortionModel = datatype_cpp.DistortionModel
 DistortionModel.__doc__ = """Enumeration of distortion models."""
 
+
 # ------- Image -------
-Image = datatype_cpp.Image
-Image.__doc__ = """Class representing an image."""
+class Image(datatype_cpp.Image):
+    """Class representing an image."""
+
+    @classmethod
+    def from_numpy(cls, data: np.ndarray, encoding=ImageEncoding.RGB) -> Image:
+        """Construct an image from a numpy array.
+
+        Args:
+            data (np.ndarray): The image data as a numpy array in the shape of (H, W, 3).
+            encoding (ImageEncoding): The image color encoding.
+
+        Returns:
+            The constructed image.
+        """
+        if data.ndim != 3:
+            raise ValueError(
+                f"Expected ndim == 3 (H, W, 3), got ndim={data.ndim}, shape={data.shape}"
+            )
+
+        image = cls()
+        image.encoding = encoding
+        image.format = ImageFormat.RAW
+
+        data_u8 = data.astype(np.uint8)
+
+        height, width, channels = data_u8.shape
+        if channels != 3:
+            raise ValueError(f"Expected channels==3, got {channels}")
+        image.height = int(height)
+        image.width = int(width)
+        image.step = int(width * channels)
+        image.data = data_u8.ravel().tolist()
+
+        return image
+
+    @classmethod
+    def from_file(cls, filepath: PathLike) -> Image:
+        """Construct an image from a file.
+
+        Args:
+            filepath (PathLike): The path to the image file.
+
+        Returns:
+            The constructed image.
+        """
+        cv_image = cv2.imread(str(filepath), cv2.IMREAD_COLOR)
+        if cv_image is None:
+            raise ValueError(f"Failed to read image: {filepath}")
+        cv_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB)
+        return cls.from_numpy(cv_image, ImageEncoding.RGB)
+
 
 CameraInfo = datatype_cpp.CameraInfo
 CameraInfo.__doc__ = """Class representing camera information."""
