2c7a1d8e943c59acd1521c9765a59f86d4433c8d
Kento Yabuuchi <moc.liamg.8y8@gmail.com>
2026-03-16 20:09:26 +0900

    feat(accelerated_image_processor): jazzy migration (#34)
    * use hpp include file for jazzy
    
    Signed-off-by: Kento Yabuuchi <kento.yabuuchi.2@tier4.jp>
    
    * cv_bridge.cpp for jazzy
    
    Signed-off-by: Kento Yabuuchi <kento.yabuuchi.2@tier4.jp>
    
    ---------
    
    Signed-off-by: Kento Yabuuchi <kento.yabuuchi.2@tier4.jp>

--- src/accelerated_image_processor/src/accelerator/rectifier.cpp
+++ src/accelerated_image_processor/src/accelerator/rectifier.cpp
@@ -8,8 +8,14 @@
 #include <nppi_support_functions.h>
 #endif
 
+#ifdef ROS_DISTRO_HUMBLE
 #include <image_geometry/pinhole_camera_model.h>
 #include <cv_bridge/cv_bridge.h>
+#else
+#include <image_geometry/pinhole_camera_model.hpp>
+#include <cv_bridge/cv_bridge.hpp>
+#endif
+
 #include <opencv2/imgcodecs.hpp>
 
 #ifdef OPENCV_CUDA_AVAILABLE
